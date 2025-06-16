947332b96bd6d08fd28983d5915e7f56b47632f8
Neil Al Hakim <nal-haki@c1r4s3.42beirut.com>
2025-06-16 18:08:25 +0300

    06 done

---
+++ CPP-06/ex02/main.cpp
@@ -0,0 +1,67 @@
+#include "Base.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+Base* generate(void) {
+    std::srand(std::time(0));
+    int random = std::rand() % 3;
+    
+    switch (random) {
+        case 0: return new A();
+        case 1: return new B();
+        case 2: return new C();
+    }
+    return NULL;
+}
+
+void identify(Base* p) {
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base& p) {
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    } catch (...) {}
+    
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    } catch (...) {}
+    
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    } catch (...) {}
+}
+
+int main() {
+    Base* random1 = generate();
+    Base* random2 = generate();
+    Base* random3 = generate();
+
+    std::cout << "Pointer identification:" << std::endl;
+    identify(random1);
+    identify(random2);
+    identify(random3);
+
+    std::cout << "\nReference identification:" << std::endl;
+    identify(*random1);
+    identify(*random2);
+    identify(*random3);
+
+    delete random1;
+    delete random2;
+    delete random3;
+
+    return 0;
+}
