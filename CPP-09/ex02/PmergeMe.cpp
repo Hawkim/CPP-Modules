68182bb51684d1042645e090abacdd925774fd9e
Neil Dell Laptop <nal-haki@Dell-Latitude.localdomain>
2025-07-29 04:11:33 +0300

    another way

--- CPP-09/ex02/PmergeMe.cpp
+++ CPP-09/ex02/PmergeMe.cpp
@@ -2,28 +2,32 @@
 #include <cstdlib>
 #include <climits>
 #include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+
+// Comparison helpers for counting
+inline bool cmpVecGreater(int a, int b, size_t& c) { ++c; return a > b; }
+inline bool cmpVecLess(int a, int b, size_t& c) { ++c; return a < b; }
+inline bool cmpDeqGreater(int a, int b, size_t& c) { ++c; return a > b; }
+inline bool cmpDeqLess(int a, int b, size_t& c) { ++c; return a < b; }
 
 PmergeMe::PmergeMe(int argc, char** argv) : vecComparisons(0), deqComparisons(0)
 {
-    if (argc < 2)
-        throw std::runtime_error("Usage: ./PmergeMe <numbers...>");
-
+    if (argc < 2) throw std::runtime_error("Usage: ./PmergeMe <numbers...>");
     std::vector<int> seen;
     for (int i = 1; i < argc; ++i)
     {
         std::string s(argv[i]);
-        if (!isPositiveInteger(s))
-            throw std::runtime_error("Error: Invalid number");
+        if (!isPositiveInteger(s)) throw std::runtime_error("Error: Invalid number");
 
         long num = atol(s.c_str());
-        if (num <= 0 || num > INT_MAX)
-            throw std::runtime_error("Error: Number out of range");
+        if (num <= 0 || num > INT_MAX) throw std::runtime_error("Error: Number out of range");
 
         int n = num;
-        for (size_t j = 0; j < seen.size(); ++j) {
-            if (seen[j] == n)
-                throw std::runtime_error("Error: Duplicate number");
-        }
+        if (std::find(seen.begin(), seen.end(), n) != seen.end())
+            throw std::runtime_error("Error: Duplicate number");
+
         seen.push_back(n);
         vec.push_back(n);
         deq.push_back(n);
@@ -41,12 +45,12 @@ void PmergeMe::process()
     std::deque<int> deqCopy(deq);
 
     clock_t startVec = clock();
-    fordJohnsonSort(vecCopy);
+    fordJohnsonSort(vecCopy, vecComparisons);
     clock_t endVec = clock();
     double timeVec = (double)(endVec - startVec) * 1e6 / CLOCKS_PER_SEC;
 
     clock_t startDeq = clock();
-    fordJohnsonSort(deqCopy);
+    fordJohnsonSort(deqCopy, deqComparisons);
     clock_t endDeq = clock();
     double timeDeq = (double)(endDeq - startDeq) * 1e6 / CLOCKS_PER_SEC;
 
@@ -56,53 +60,43 @@ void PmergeMe::process()
     std::cout << "Time to process " << deq.size() << " elements with std::deque: " << timeDeq << " us\n";
     std::cout << "Vector comparisons: " << vecComparisons << "\n";
     std::cout << "Deque comparisons: " << deqComparisons << "\n";
-    
-    printSortCheck(deqCopy);
-    printSortCheck(vecCopy);
 
+    printSortCheck(vecCopy);
+    printSortCheck(deqCopy);
 }
 
 bool PmergeMe::isPositiveInteger(const std::string& s) const
 {
-    if (s.empty()) 
-        return false;
+    if (s.empty()) return false;
     for (std::string::size_type i = 0; i < s.size(); ++i)
-        if (!isdigit(s[i])) 
-            return false;
+        if (!isdigit(s[i])) return false;
     return true;
 }
 
-void PmergeMe::printContainer(const std::vector<int>& vec) const
+void PmergeMe::printContainer(const std::vector<int>& v) const
 {
-    for (std::vector<int>::size_type i = 0; i < vec.size(); ++i)
-    {
-        std::cout << vec[i];
-        if (i + 1 < vec.size()) 
-            std::cout << " ";
+    for (size_t i = 0; i < v.size(); ++i) {
+        std::cout << v[i];
+        if (i + 1 < v.size()) std::cout << " ";
     }
     std::cout << std::endl;
 }
 
-void PmergeMe::printContainer(const std::deque<int>& deq) const
+void PmergeMe::printContainer(const std::deque<int>& d) const
 {
-    for (std::deque<int>::size_type i = 0; i < deq.size(); ++i)
-    {
-        std::cout << deq[i];
-        if (i + 1 < deq.size()) 
-            std::cout << " ";
+    for (size_t i = 0; i < d.size(); ++i) {
+        std::cout << d[i];
+        if (i + 1 < d.size()) std::cout << " ";
     }
     std::cout << std::endl;
 }
 
 int PmergeMe::jacobsthal(int n) const
 {
-    if (n == 0) 
-        return 0;
-    if (n == 1) 
-        return 1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
     int a = 0, b = 1, i = 2, tmp;
-    while (i <= n)
-    {
+    while (i <= n) {
         tmp = b + 2 * a;
         a = b;
         b = tmp;
@@ -111,254 +105,149 @@ int PmergeMe::jacobsthal(int n) const
     return b;
 }
 
-std::vector<int> PmergeMe::generateJacobsthalSequence(int pendSize) const
+std::vector<int> PmergeMe::generateJacobsthalSequence(int size) const
 {
     std::vector<int> seq;
-    int i = 2;
-    while (true)
-    {
+    for (int i = 2;; ++i) {
         int j = jacobsthal(i);
-        if (j > pendSize)
-            break;
+        if (j > size) break;
         seq.push_back(j);
-        ++i;
     }
     return seq;
 }
 
 std::vector<size_t> PmergeMe::generateInsertionOrder(int pendSize) const
 {
-    std::vector<int> jacobSeq = generateJacobsthalSequence(pendSize);
+    std::vector<int> jacob = generateJacobsthalSequence(pendSize);
     std::vector<size_t> order;
     int prev = 0;
-    
-    for (std::vector<int>::size_type i = 0; i < jacobSeq.size(); ++i)
-    {
-        for (int j = jacobSeq[i] - 1; j >= prev; --j)
-        {
-            if ((size_t)j < (size_t)pendSize)
-                order.push_back((size_t)j);
-        }
-        prev = jacobSeq[i];
+    for (size_t i = 0; i < jacob.size(); ++i) {
+        for (int j = jacob[i] - 1; j >= prev; --j)
+            if ((size_t)j < (size_t)pendSize) order.push_back(j);
+        prev = jacob[i];
     }
     for (int j = pendSize - 1; j >= prev; --j)
-    {
-        order.push_back((size_t)j);
-    }
+        order.push_back(j);
     return order;
 }
 
-size_t PmergeMe::binarySearch(const std::vector<int>& vec, int value, size_t left, size_t right)
+// Binary search template
+template<typename Container>
+size_t PmergeMe::binarySearch(const Container& c, int value, size_t left, size_t right, size_t& count, bool isVec)
 {
-    while (left < right)
-    {
+    while (left < right) {
         size_t mid = left + (right - left) / 2;
-        ++vecComparisons;
-        if (vec[mid] < value)
+        bool less = isVec ? cmpVecLess(c[mid], value, count) : cmpDeqLess(c[mid], value, count);
+        if (less)
             left = mid + 1;
         else
             right = mid;
     }
-    if (left < vec.size() && vec[left] < value){
-    ++vecComparisons;
-        return left + 1;
-    }
     return left;
 }
 
-size_t PmergeMe::binarySearch(const std::deque<int>& deq, int value, size_t left, size_t right)
+void PmergeMe::fordJohnsonSort(std::vector<int>& vec, size_t& cmpCount)
 {
-    while (left < right)
-    {
-        size_t mid = left + (right - left) / 2;
-        ++deqComparisons;
-        if (deq[mid] < value)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-    if (left < deq.size() && deq[left] < value){
-      ++deqComparisons;
-        return left + 1;
-    }
-      
-    return left;
-}
+    if (vec.size() <= 1) return;
 
-void PmergeMe::fordJohnsonSort(std::vector<int>& vec)
-{
-    if (vec.size() <= 1) 
-        return;
-
-    bool hasOdd = (vec.size() % 2 != 0);
+    bool hasOdd = vec.size() % 2;
     int odd = -1;
-    if (hasOdd)
-    {
-        odd = vec.back();
-        vec.pop_back();
-    }
+    if (hasOdd) { odd = vec.back(); vec.pop_back(); }
 
-    std::vector<std::pair<int, int> > pairs;
-    for (size_t i = 0; i + 1 < vec.size(); i += 2)
-    {
-        ++vecComparisons;
-        if (vec[i] > vec[i + 1])
-            pairs.push_back(std::make_pair(vec[i], vec[i + 1]));
+    std::vector< std::pair<int, int> > pairs;
+    for (size_t i = 0; i + 1 < vec.size(); i += 2) {
+        if (cmpVecGreater(vec[i], vec[i+1], cmpCount))
+            pairs.push_back(std::make_pair(vec[i], vec[i+1]));
         else
-            pairs.push_back(std::make_pair(vec[i + 1], vec[i]));
+            pairs.push_back(std::make_pair(vec[i+1], vec[i]));
     }
 
     std::vector<int> mainChain;
-    for (size_t i = 0; i < pairs.size(); ++i)
-    {
-        mainChain.push_back(pairs[i].first);
-    }
-    fordJohnsonSort(mainChain);
-
-    std::vector<std::pair<int, int> > winners;
-    for (size_t i = 0; i < pairs.size(); ++i)
-    {
-        winners.push_back(std::make_pair(pairs[i].first, pairs[i].second));
-    }
+    for (size_t i = 0; i < pairs.size(); ++i) mainChain.push_back(pairs[i].first);
+    fordJohnsonSort(mainChain, cmpCount);
 
     std::vector<int> pend;
     for (size_t i = 0; i < mainChain.size(); ++i)
-    {
-        for (size_t j = 0; j < winners.size(); ++j)
-        {
-            if (winners[j].first == mainChain[i])
-            {
-                pend.push_back(winners[j].second);
-                break;
-            }
-        }
-    }
+        for (size_t j = 0; j < pairs.size(); ++j)
+            if (pairs[j].first == mainChain[i]) { pend.push_back(pairs[j].second); break; }
 
     std::vector<size_t> partner(mainChain.size());
-    for (size_t i = 0; i < mainChain.size(); ++i)
-    {
-        partner[i] = i;
-    }
+    for (size_t i = 0; i < mainChain.size(); ++i) partner[i] = i;
 
-    if (!pend.empty())
-    {
+    if (!pend.empty()) {
         mainChain.insert(mainChain.begin(), pend[0]);
-        for (size_t i = 0; i < partner.size(); ++i)
-        {
-            ++partner[i];
-        }
+        for (size_t i = 0; i < partner.size(); ++i) ++partner[i];
     }
 
     std::vector<size_t> order = generateInsertionOrder(pend.size());
-    for (size_t i = 0; i < order.size(); ++i)
-    {
+    for (size_t i = 0; i < order.size(); ++i) {
         size_t idx = order[i];
-        if (idx == 0 || idx >= pend.size()) 
-            continue;
+        if (idx == 0 || idx >= pend.size()) continue;
         int val = pend[idx];
-        size_t insertPos = binarySearch(mainChain, val, 0, partner[idx] - 1);
-        mainChain.insert(mainChain.begin() + insertPos, val);
+        size_t limit = partner[idx]; // STRICT partner bound
+        size_t pos = binarySearch(mainChain, val, 0, limit, cmpCount, true);
+        mainChain.insert(mainChain.begin() + pos, val);
         for (size_t j = 0; j < partner.size(); ++j)
-        {
-            if (partner[j] >= insertPos)
-                ++partner[j];
-        }
+            if (partner[j] >= pos) ++partner[j];
     }
 
-    if (hasOdd)
-    {
-        size_t pos = binarySearch(mainChain, odd, 0, mainChain.size() - 1);
+    if (hasOdd) {
+        size_t pos = binarySearch(mainChain, odd, 0, mainChain.size(), cmpCount, true);
         mainChain.insert(mainChain.begin() + pos, odd);
     }
 
     vec = mainChain;
 }
 
-void PmergeMe::fordJohnsonSort(std::deque<int>& deq)
+void PmergeMe::fordJohnsonSort(std::deque<int>& deq, size_t& cmpCount)
 {
-    if (deq.size() <= 1) 
-        return;
+    if (deq.size() <= 1) return;
 
-    bool hasOdd = (deq.size() % 2 != 0);
+    bool hasOdd = deq.size() % 2;
     int odd = -1;
-    if (hasOdd)
-    {
-        odd = deq.back();
-        deq.pop_back();
-    }
+    if (hasOdd) { odd = deq.back(); deq.pop_back(); }
 
-    std::vector<std::pair<int, int> > pairs;
-    for (size_t i = 0; i + 1 < deq.size(); i += 2)
-    {
-        ++deqComparisons;
-        if (deq[i] > deq[i + 1])
-            pairs.push_back(std::make_pair(deq[i], deq[i + 1]));
+    std::vector< std::pair<int, int> > pairs;
+    for (size_t i = 0; i + 1 < deq.size(); i += 2) {
+        if (cmpDeqGreater(deq[i], deq[i+1], cmpCount))
+            pairs.push_back(std::make_pair(deq[i], deq[i+1]));
         else
-            pairs.push_back(std::make_pair(deq[i + 1], deq[i]));
+            pairs.push_back(std::make_pair(deq[i+1], deq[i]));
     }
 
     std::deque<int> mainChain;
-    for (size_t i = 0; i < pairs.size(); ++i)
-    {
-        mainChain.push_back(pairs[i].first);
-    }
-    fordJohnsonSort(mainChain);
-
-    std::vector<std::pair<int, int> > winners;
-    for (size_t i = 0; i < pairs.size(); ++i)
-    {
-        winners.push_back(std::make_pair(pairs[i].first, pairs[i].second));
-    }
+    for (size_t i = 0; i < pairs.size(); ++i) mainChain.push_back(pairs[i].first);
+    fordJohnsonSort(mainChain, cmpCount);
 
     std::deque<int> pend;
     for (size_t i = 0; i < mainChain.size(); ++i)
-    {
-        for (size_t j = 0; j < winners.size(); ++j)
-        {
-            if (winners[j].first == mainChain[i])
-            {
-                pend.push_back(winners[j].second);
-                break;
-            }
-        }
-    }
+        for (size_t j = 0; j < pairs.size(); ++j)
+            if (pairs[j].first == mainChain[i]) { pend.push_back(pairs[j].second); break; }
 
     std::vector<size_t> partner(mainChain.size());
-    for (size_t i = 0; i < mainChain.size(); ++i)
-    {
-        partner[i] = i;
-    }
+    for (size_t i = 0; i < mainChain.size(); ++i) partner[i] = i;
 
-    if (!pend.empty())
-    {
+    if (!pend.empty()) {
         mainChain.push_front(pend[0]);
-        for (size_t i = 0; i < partner.size(); ++i)
-        {
-            ++partner[i];
-        }
+        for (size_t i = 0; i < partner.size(); ++i) ++partner[i];
     }
 
     std::vector<size_t> order = generateInsertionOrder(pend.size());
-    for (size_t i = 0; i < order.size(); ++i)
-    {
+    for (size_t i = 0; i < order.size(); ++i) {
         size_t idx = order[i];
-        if (idx == 0 || idx >= pend.size()) 
-            continue;
+        if (idx == 0 || idx >= pend.size()) continue;
         int val = pend[idx];
-        size_t insertPos = binarySearch(mainChain, val, 0, partner[idx] - 1);
-        mainChain.insert(mainChain.begin() + insertPos, val);
+        size_t limit = partner[idx];
+        size_t pos = binarySearch(mainChain, val, 0, limit, cmpCount, false);
+        mainChain.insert(mainChain.begin() + pos, val);
         for (size_t j = 0; j < partner.size(); ++j)
-        {
-            if (partner[j] >= insertPos)
-                ++partner[j];
-        }
+            if (partner[j] >= pos) ++partner[j];
     }
 
-    if (hasOdd)
-    {
-        size_t pos = binarySearch(mainChain, odd, 0, mainChain.size() - 1);
+    if (hasOdd) {
+        size_t pos = binarySearch(mainChain, odd, 0, mainChain.size(), cmpCount, false);
         mainChain.insert(mainChain.begin() + pos, odd);
     }
-    
+
     deq = mainChain;
 }
--- CPP-09/ex02/PmergeMe.hpp
+++ CPP-09/ex02/PmergeMe.hpp
@@ -1,12 +1,12 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <iostream>
 #include <vector>
 #include <deque>
 #include <string>
-#include <iostream>
 #include <stdexcept>
-#include <climits>
+#include <ctime>
 
 class PmergeMe {
 public:
@@ -18,39 +18,46 @@ public:
 private:
     std::vector<int> vec;
     std::deque<int> deq;
+
     size_t vecComparisons;
     size_t deqComparisons;
 
     bool isPositiveInteger(const std::string& s) const;
+
     void printContainer(const std::vector<int>& vec) const;
     void printContainer(const std::deque<int>& deq) const;
+
+    void printSortCheck(const std::vector<int>& vec) const {
+        for (size_t i = 1; i < vec.size(); ++i) {
+            if (vec[i] < vec[i - 1]) {
+                std::cout << "✘ Not sorted" << std::endl;
+                return;
+            }
+        }
+        std::cout << "✔ Sorted" << std::endl;
+    }
+
+    void printSortCheck(const std::deque<int>& deq) const {
+        for (size_t i = 1; i < deq.size(); ++i) {
+            if (deq[i] < deq[i - 1]) {
+                std::cout << "✘ Not sorted" << std::endl;
+                return;
+            }
+        }
+        std::cout << "✔ Sorted" << std::endl;
+    }
 
     int jacobsthal(int n) const;
     std::vector<int> generateJacobsthalSequence(int pendSize) const;
     std::vector<size_t> generateInsertionOrder(int pendSize) const;
 
-    size_t binarySearch(const std::vector<int>& vec, int value, size_t left, size_t right);
-    size_t binarySearch(const std::deque<int>& deq, int value, size_t left, size_t right);
+    // New: templated binary search with partner restriction
+    template<typename Container>
+    size_t binarySearch(const Container& c, int value, size_t left, size_t right, size_t& count, bool isVec);
 
-    void fordJohnsonSort(std::vector<int>& vec);
-    void fordJohnsonSort(std::deque<int>& deq);
-
-
-template <typename Container>
-void printSortCheck(const Container& c) const
-{
-    for (size_t i = 1; i < c.size(); ++i)
-    {
-        if (c[i - 1] > c[i])
-        {
-            std::cout << "\033[1;31m✘ Not sorted\033[0m" << std::endl;
-            return;
-        }
-    }
-    std::cout << "\033[1;32m✔ Sorted\033[0m" << std::endl;
-}
-
-
+    // Updated Ford-Johnson sort signatures (with comparison counter reference)
+    void fordJohnsonSort(std::vector<int>& vec, size_t& cmpCount);
+    void fordJohnsonSort(std::deque<int>& deq, size_t& cmpCount);
 };
 
 #endif
